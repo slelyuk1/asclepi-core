2489f6b48e0271ba5b36e6676dd7b57df580b1b9
Oleksandr Leliuk <slelyuk1@gmail.com>
2021-03-20 16:50:49 +0200

    Fixed diagnosis signature

--- src/main/java/com/jupiter/asclepi/core/rest/controller/DiagnosisController.java
+++ src/main/java/com/jupiter/asclepi/core/rest/controller/DiagnosisController.java
@@ -3,6 +3,7 @@ package com.jupiter.asclepi.core.rest.controller;
 import com.jupiter.asclepi.core.helper.api.business.controller.CrudController;
 import com.jupiter.asclepi.core.model.request.disease.diagnosis.CreateDiagnosisRequest;
 import com.jupiter.asclepi.core.model.request.disease.diagnosis.EditDiagnosisRequest;
+import com.jupiter.asclepi.core.model.request.disease.history.GetDiseaseHistoryRequest;
 import com.jupiter.asclepi.core.model.response.disease.DiagnosisInfo;
 
 import java.math.BigInteger;
@@ -10,5 +11,5 @@ import java.util.List;
 
 public interface DiagnosisController extends CrudController<BigInteger, CreateDiagnosisRequest, EditDiagnosisRequest, DiagnosisInfo> {
 
-    List<DiagnosisInfo> getForDiseaseHistory(BigInteger diseaseHistoryId);
+    List<DiagnosisInfo> getForDiseaseHistory(GetDiseaseHistoryRequest request);
 }
