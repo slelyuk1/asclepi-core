69b316cbdb457d4ea19e1426cbbb466152f63bde
Whitewizzard <rj_dio70@yahoo.com>
2021-03-28 14:25:45 +0300

    impl analysis controller

--- src/main/java/com/jupiter/asclepi/core/rest/controller/AnalysisController.java
+++ src/main/java/com/jupiter/asclepi/core/rest/controller/AnalysisController.java
@@ -3,6 +3,7 @@ package com.jupiter.asclepi.core.rest.controller;
 import com.jupiter.asclepi.core.helper.api.business.controller.CrudController;
 import com.jupiter.asclepi.core.model.request.disease.analysis.CreateAnalysisRequest;
 import com.jupiter.asclepi.core.model.request.disease.analysis.EditAnalysisRequest;
+import com.jupiter.asclepi.core.model.request.disease.analysis.GetAnalysisRequest;
 import com.jupiter.asclepi.core.model.request.disease.history.GetDiseaseHistoryRequest;
 import com.jupiter.asclepi.core.model.request.disease.visit.GetVisitRequest;
 import com.jupiter.asclepi.core.model.response.disease.AnalysisInfo;
@@ -10,7 +11,7 @@ import com.jupiter.asclepi.core.model.response.disease.AnalysisInfo;
 import java.math.BigInteger;
 import java.util.List;
 
-public interface AnalysisController extends CrudController<BigInteger, CreateAnalysisRequest, EditAnalysisRequest, AnalysisInfo> {
+public interface AnalysisController extends CrudController<GetAnalysisRequest, CreateAnalysisRequest, EditAnalysisRequest, AnalysisInfo> {
 
     List<AnalysisInfo> getAnalysisForVisit(GetVisitRequest visitGetter);
 
