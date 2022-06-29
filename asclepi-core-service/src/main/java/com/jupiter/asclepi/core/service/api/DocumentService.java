f2e417d4fa5258c2d30545b081ee5b08aabe3746
Oleksandr Leliuk <slelyuk1@gmail.com>
2022-06-29 19:23:16 +0300

    Implemented Document functionality

--- asclepi-core-service/src/main/java/com/jupiter/asclepi/core/service/api/DocumentService.java
+++ asclepi-core-service/src/main/java/com/jupiter/asclepi/core/service/api/DocumentService.java
@@ -7,7 +7,5 @@ import com.jupiter.asclepi.core.service.helper.api.CrudService;
 
 import java.math.BigInteger;
 
-// todo implement service (Viktor Muratov) (see com.jupiter.asclepi.core.service.api.EmployeeService)
-@SuppressWarnings("unused")
 public interface DocumentService extends CrudService<BigInteger, CreateDocumentRequest, EditDocumentRequest, Document, Boolean> {
 }
